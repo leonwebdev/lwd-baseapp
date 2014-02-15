65f1ead549ceae7e8cd213235a6661309bfb5f66
Leon Web Dev <leon@marcetleon.com>
2014-02-15 10:28:47 +0100

    add config file global

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -12,5 +12,20 @@
  */
 
 return array(
-    // ...
+    'lwd-send-email-email' => 'contact@localhost.com',
+    'lwd-send-email-name' => 'lwd',
+    
+    'session' => array(
+        'config' => array(
+            'class' => 'Zend\Session\Config\SessionConfig',
+            'options' => array(
+                'name' => 'lwd',
+            ),
+        ),
+        'storage' => 'Zend\Session\Storage\SessionArrayStorage',
+        'validators' => array(
+            'Zend\Session\Validator\RemoteAddr',
+            'Zend\Session\Validator\HttpUserAgent',
+        ),
+    ),
 );
